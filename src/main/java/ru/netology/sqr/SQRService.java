b307fe099b536517e157480c2c398b9cbb78b34c
Valery Buz <Ajven@yandex.ru>
2022-01-08 14:15:25 +0300

    adding changes

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,11 +2,11 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public static int sqrCalculate (int lowBand, int hiBand) {
+    public static int sqrCalculate(int lowBand, int hiBand) {
         int counter = 0;
-        for (int i = 10; i <=99; i++) {
+        for (int i = 10; i <= 99; i++) {
             int j = i * i;
-            if (j>=lowBand && j <=hiBand) {
+            if (j >= lowBand && j <= hiBand) {
                 counter++;
                 System.out.println("В границах диапазона " + i + " общее число в диапазоне = " + counter);
             }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,14 +8,14 @@ class SQRServiceTest {
 
     @Test
     void sqrCalculate() {
-            SQRService service = new SQRService();
+        SQRService service = new SQRService();
 
-            int myDown = 200;
-            int myUp = 300;
-            int expected = 3;
-            int myResult = SQRService.sqrCalculate(myDown, myUp);
+        int myDown = 200;
+        int myUp = 300;
+        int expected = 3;
+        int myResult = SQRService.sqrCalculate(myDown, myUp);
 
         assertEquals(expected, myResult);
-        }
-
     }
+
+}
